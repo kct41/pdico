8f3dd5b7c34c64076c8911049a7f7d82896ba3b3
kimsomi96 <kimsomi@pdic.co.kr>
2022-11-18 17:42:44 +0900

    - 11.18 중간 백업

--- src/Project.js
+++ src/Project.js
@@ -3,6 +3,7 @@ import TopBar from "./component/TopBar";
 import ProjectCard from "./component/ProjectCard";
 import ProjectMember from "./component/ProjectMember";
 import WeekdayBox from "./component/WeekdayBox";
+import Scheduler from "./component/Scheduler/Scheduler";
 
 function Project(){
     return(
@@ -17,88 +18,88 @@ function Project(){
                     <ProjectCard name="PDICO 화면 구현" date="2022년 10월 1일 ~ 2022년 11월 31일"/>
                 </div>
                 <div className="project__detail__area">
-                    <h2>2022년 10월</h2>
                     <div className="container">
-                        <div className="member__box">
-                            <ProjectMember username="김용희" role="PM / 백엔드"/>
-                            <ProjectMember username="김소미" role="프론트엔드"/>
-                            <ProjectMember username="최정민" role="프론트엔드"/>
-                            <ProjectMember username="임소현" role="디자인"/>
-                            <ProjectMember username="이진욱" role="백엔드"/>
-                            <ProjectMember username="이효재" role="백엔드"/>
-                        </div>
-                        <div className="detail__box">
-                            <div className="box">
-                                <div className="date">
-                                    <h5>목</h5>
-                                    <p>20일</p>
-                                </div>
-                                <div className="flex__box">
-                                    <div></div>
-                                    <div></div>
-                                </div>
-                            </div>
-                            <div className="box">
-                                <div className="date">
-                                    <h5>금</h5>
-                                    <p>21일</p>
-                                </div>
-                                <div className="flex__box today">
-                                    <div></div>
-                                    <div></div>
-                                </div>
-                            </div>
-                            <div className="box">
-                                <div className="date">
-                                    <h5>토</h5>
-                                    <p>22일</p>
-                                </div>
-                                <div className="flex__box">
-                                    <div></div>
-                                    <div className="weekend"></div>
-                                </div>
-                            </div>
-                            <div className="box">
-                                <div className="date">
-                                    <h5>일</h5>
-                                    <p>23일</p>
-                                </div>
-                                <div className="flex__box">
-                                    <div className="weekend"></div>
-                                    <div className="weekend"></div>
-                                </div>
-                            </div>
-                            <div className="box">
-                                <div className="date">
-                                    <h5>월</h5>
-                                    <p>24일</p>
-                                </div>
-                                <div className="flex__box">
-                                    <div className="weekend"></div>
-                                    <div></div>
-                                </div>
-                            </div>
-                            <div className="box">
-                                <div className="date">
-                                    <h5>화</h5>
-                                    <p>25일</p>
-                                </div>
-                                <div className="flex__box">
-                                    <div></div>
-                                    <div></div>
-                                </div>
-                            </div>
-                            <div className="box">
-                                <div className="date">
-                                    <h5>수</h5>
-                                    <p>26일</p>
-                                </div>
-                                <div className="flex__box">
-                                    <div></div>
-                                    <div></div>
-                                </div>
-                            </div>
-                        </div>
+                        <Scheduler/>
+                        {/*<div className="member__box">*/}
+                        {/*    <ProjectMember username="김용희" role="PM / 백엔드"/>*/}
+                        {/*    <ProjectMember username="김소미" role="프론트엔드"/>*/}
+                        {/*    <ProjectMember username="최정민" role="프론트엔드"/>*/}
+                        {/*    <ProjectMember username="임소현" role="디자인"/>*/}
+                        {/*    <ProjectMember username="이진욱" role="백엔드"/>*/}
+                        {/*    <ProjectMember username="이효재" role="백엔드"/>*/}
+                        {/*</div>*/}
+                        {/*<div className="detail__box">*/}
+                        {/*    <div className="box">*/}
+                        {/*        <div className="date">*/}
+                        {/*            <h5>목</h5>*/}
+                        {/*            <p>20일</p>*/}
+                        {/*        </div>*/}
+                        {/*        <div className="flex__box">*/}
+                        {/*            <div></div>*/}
+                        {/*            <div></div>*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="box">*/}
+                        {/*        <div className="date">*/}
+                        {/*            <h5>금</h5>*/}
+                        {/*            <p>21일</p>*/}
+                        {/*        </div>*/}
+                        {/*        <div className="flex__box today">*/}
+                        {/*            <div></div>*/}
+                        {/*            <div></div>*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="box">*/}
+                        {/*        <div className="date">*/}
+                        {/*            <h5>토</h5>*/}
+                        {/*            <p>22일</p>*/}
+                        {/*        </div>*/}
+                        {/*        <div className="flex__box">*/}
+                        {/*            <div></div>*/}
+                        {/*            <div className="weekend"></div>*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="box">*/}
+                        {/*        <div className="date">*/}
+                        {/*            <h5>일</h5>*/}
+                        {/*            <p>23일</p>*/}
+                        {/*        </div>*/}
+                        {/*        <div className="flex__box">*/}
+                        {/*            <div className="weekend"></div>*/}
+                        {/*            <div className="weekend"></div>*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="box">*/}
+                        {/*        <div className="date">*/}
+                        {/*            <h5>월</h5>*/}
+                        {/*            <p>24일</p>*/}
+                        {/*        </div>*/}
+                        {/*        <div className="flex__box">*/}
+                        {/*            <div className="weekend"></div>*/}
+                        {/*            <div></div>*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="box">*/}
+                        {/*        <div className="date">*/}
+                        {/*            <h5>화</h5>*/}
+                        {/*            <p>25일</p>*/}
+                        {/*        </div>*/}
+                        {/*        <div className="flex__box">*/}
+                        {/*            <div></div>*/}
+                        {/*            <div></div>*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="box">*/}
+                        {/*        <div className="date">*/}
+                        {/*            <h5>수</h5>*/}
+                        {/*            <p>26일</p>*/}
+                        {/*        </div>*/}
+                        {/*        <div className="flex__box">*/}
+                        {/*            <div></div>*/}
+                        {/*            <div></div>*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
                     </div>
                 </div> 
             </div>
---
+++ src/component/Scheduler/DemoData.js
@@ -0,0 +1,99 @@
+const DemoData = {
+    resources: [
+        {
+            id: "r1",
+            name: "Phoebe"
+        },
+        {
+            id: "r2",
+            name: "Brian"
+        },
+        {
+            id: "r3",
+            name: "Jerry"
+        }
+    ],
+    events: [
+        {
+            id: 1,
+            start: "2022-11-09",
+            end: "2022-11-15",
+            resourceId: "r1",
+            title: "프디코 화면 구현",
+            bgColor: "#1AC2B1",
+            showPopover: false
+        },
+        {
+            id: 2,
+            start: "2022-11-15",
+            end: "2022-11-18",
+            resourceId: "r1",
+            title: "프디코 DB설계",
+            bgColor: "#1AC2B1",
+            showPopover: false
+        },
+        {
+            id: 3,
+            start: "2020-11-12",
+            end: "2020-11-19",
+            resourceId: "r3",
+            title: "Sports Event",
+            bgColor: "#1AC2B1",
+            showPopover: false
+        },
+        {
+            id: 5,
+            start: "2020-11-22",
+            end: "2020-11-26",
+            resourceId: "r3",
+            title: "Samsung Event",
+            bgColor: "#F3A023",
+            movable: false
+        },
+        {
+            id: 6,
+            start: "2020-11-18",
+            end: "2020-11-20",
+            resourceId: "r3",
+            title: "Apple Event",
+            bgColor: "#F3A023",
+            movable: false
+        },
+        {
+            id: 7,
+            start: "2020-11-20",
+            end: "2020-11-23",
+            resourceId: "r1",
+            bgColor: "#F3A023",
+            title: "Google Event"
+        },
+        {
+            id: 8,
+            start: "2020-11-27",
+            end: "2020-11-29",
+            resourceId: "r3",
+            title: "Mi Event",
+            bgColor: "#F3A023",
+            movable: false
+        },
+        {
+            id: 6,
+            start: "2020-11-17",
+            end: "2020-11-19",
+            resourceId: "r1",
+            title: "iOS Event",
+            bgColor: "#F3A023",
+            movable: false
+        },
+        {
+            id: 7,
+            start: "2020-11-24",
+            end: "2020-11-30",
+            resourceId: "r1",
+            bgColor: "#F3A023",
+            title: "LG Event"
+        }
+    ]
+};
+
+export default DemoData;
